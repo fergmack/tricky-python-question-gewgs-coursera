6b65166815bdaa90df6740fa5299a0da473f06ec
fergmack <33000542+fergmack@users.noreply.github.com>
2020-01-30 22:55:39 +0000

    Update problem.py

--- problem.py
+++ problem.py
@@ -29,3 +29,54 @@ def smallest_prime_factor(x):
 
 print(smallest_prime_factor(12)) # should be 2
 print(smallest_prime_factor(15)) # should be 3
+
+# Fill in the blanks to make the print_prime_factors function print all the prime factors of a number. A prime factor is a number that is prime and divides another without a remainder.
+# Note the instructions on this are a bit confusing
+def print_prime_factors(number):
+  # Start with two, which is the first prime
+  factor = 2
+  # Keep going until the factor is larger than the number
+  while factor <= number:
+    # Check if factor is a divisor of number
+    if number % factor == 0:
+      # If it is, print it and divide the original number
+      print(factor)
+      number = number / factor
+    else:
+      # If it's not, increment the factor by one
+      factor += 1
+  return "Done"
+
+print_prime_factors(100) # Should print 2,2,5,5
+
+# The following code can lead to an infinite loop. Fix the code so that it can finish successfully for all numbers.
+# Note: Try running your function with the number 0 as the input, and see what you get!
+# FIXED
+def is_power_of_two(n):
+  # Check if the number can be divided by two without a remainder
+  while n % 2 == 0 and n != 0:
+    n = n / 2
+  # If after dividing by two the number is 1, it's a power of two
+  if n == 1:
+    return True
+  return False
+  
+is_power_of_two(0)
+
+# Fill in the empty function so that it returns the sum of all the divisors of a number, without including it. A divisor is a number that divides into another without a remainder.
+def sum_divisors(n):
+  i = 1
+  sum_no = 0
+  while i < n:
+    if n % i == 0:
+      sum_no += i
+    i += 1
+  return sum_no
+
+
+print(sum_divisors(6)) # Should be 1+2+3=6
+print(sum_divisors(12)) # Should be 1+2+3+4+6=16
+
+
+
+
