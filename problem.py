d7115d35069c4086b6725c882f2e5bf59e450456
fergmack <33000542+fergmack@users.noreply.github.com>
2020-02-18 21:23:58 +0000

    Update problem.py

--- problem.py
+++ problem.py
@@ -271,3 +271,12 @@ def initials(phrase):
 print(initials("Universal Serial Bus")) # Should be: USB
 print(initials("local area network")) # Should be: LAN
 print(initials("Operating system")) # Should be: OS
+
+# Modify the student_grade function using the format method, so that it returns the phrase "X received Y% on the exam". 
+# For example, student_grade("Reed", 80) should return "Reed received 80% on the exam".
+def student_grade(name, grade):
+	return "{name} received {grade}% on the exam".format(name=name, grade=grade)
+
+print(student_grade("Reed", 80))
+print(student_grade("Paige", 92))
+print(student_grade("Jesse", 85))
