751fe9421cfa33a12bc74dd01459abcdfabbe96c
fergmack <33000542+fergmack@users.noreply.github.com>
2020-02-25 20:57:52 +0000

    Update problem.py

--- problem.py
+++ problem.py
@@ -330,3 +330,11 @@ def skip_elements(elements):
 
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
 print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+# Complete the skip_elements function to return every other element from the list, this time using a 
+# list comprehension to generate the new list based on the previous one, where elements in odd positions are skipped.
+def skip_elements(elements):
+  return [element for index, element in enumerate(elements) if index % 2 == 0 ]
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
