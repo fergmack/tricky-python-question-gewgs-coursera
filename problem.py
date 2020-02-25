b55998c73f145cd39c3617d6a9e73854773ebf76
fergmack <33000542+fergmack@users.noreply.github.com>
2020-02-25 20:30:24 +0000

    Update problem.py

--- problem.py
+++ problem.py
@@ -317,3 +317,16 @@ def guest_list(guests):
 		print("{} is {} years old and works as {}.".format(name, age, job))
 
 guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
+
+# Try out the enumerate function for yourself in this quick exercise. Complete the skip_elements function to return every other 
+# element from the list, this time using the enumerate function to check if an element is on an even position or an odd position.
+
+def skip_elements(elements):
+  skipped =[]
+  for index, element in enumerate(elements):
+    if index % 2 == 0:
+      skipped.append(element)
+  return skipped
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
