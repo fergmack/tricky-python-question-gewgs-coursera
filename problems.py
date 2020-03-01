f6b089d3d7b7afdd6d669ef60b4a2b60398a77b4
fergmack <33000542+fergmack@users.noreply.github.com>
2020-03-01 15:35:17 +0000

    Update and rename problem.py to problems.py

--- problems.py
+++ problems.py
@@ -355,3 +355,18 @@ for c in txt:
 
 for c in letter_counts.keys():
     print(c + ": " + str(letter_counts[c]) + " occurrences")
+
+# The "toc" dictionary represents the table of contents for a book. Fill in the blanks to do the following: 
+# 1) Add an entry for Epilogue on page 39. 2) Change the page number for Chapter 3 to 24. 3)
+# Display the new dictionary contents. 4) Display True if there is Chapter 5, False if there isn't.
+
+toc = {"Introduction":1, "Chapter 1":4, "Chapter 2":11, "Chapter 3":25, "Chapter 4":30}
+# Epilogue starts on page 39
+# Chapter 3 now starts on page 24
+# What are the current contents of the dictionary?
+# Is there a Chapter 5?
+
+toc['Epilogue'] = 39
+toc['Chapter 3'] = 24
+print(toc)
+print('Chapter 5' in toc)
