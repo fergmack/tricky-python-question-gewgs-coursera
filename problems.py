8e0606827ca83d1537961920ee4fca795f5f7a6c
fergmack <33000542+fergmack@users.noreply.github.com>
2020-03-01 15:51:27 +0000

    Update problems.py

--- problems.py
+++ problems.py
@@ -370,3 +370,11 @@ toc['Epilogue'] = 39
 toc['Chapter 3'] = 24
 print(toc)
 print('Chapter 5' in toc)
+
+Now, it's your turn! Have a go at iterating over a dictionary!
+
+# Complete the code to iterate through the keys and values of the cool_beasts dictionary. 
+# Remember that the items method returns a tuple of key, value for each element in the dictionary.
+cool_beasts = {"octopuses":"tentacles", "dolphins":"fins", "rhinos":"horns"}
+for key, value in cool_beasts.items():
+    print("{} have {}".format(key, value))
