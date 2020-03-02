3fb49567a0a7816af1d98225208ac6de11b382f4
fergmack <33000542+fergmack@users.noreply.github.com>
2020-03-02 20:08:05 +0000

    Update problems.py

--- problems.py
+++ problems.py
@@ -388,6 +388,7 @@ for item in wardrobe:
   for color in wardrobe[item]:
     print("{} {}".format(color, item))
 
+# *** Go over this - cause a lot of trouble ***
 # The groups_per_user function receives a dictionary, which contains group names with the list of users.
 # Users can belong to multiple groups. Fill in the blanks to return a dictionary with the users as keys and a list
 # of their groups as values.
