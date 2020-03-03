bc807974fedaa9590de2c8599abc5dd723b5a6ea
fergmack <33000542+fergmack@users.noreply.github.com>
2020-03-03 21:01:08 +0000

    Update problems.py

--- problems.py
+++ problems.py
@@ -370,7 +370,83 @@ toc['Epilogue'] = 39
 toc['Chapter 3'] = 24
 print(toc)
 print('Chapter 5' in toc)
+# ---------------------------
+# Lists - quite tricky problems
+# Given a list of filenames, we want to rename all the files with the extension hpp to the extension h by generating a list of tuples of the form (old_name, new_name).
 
+# That is, given the following list of filenames
+
+# filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+
+# complete the starter code to generate the following newfilenames list of tuples
+
+# newfilenames = [('program.c', 'program.c'), ('stdio.hpp', 'stdio.h'), ('sample.hpp', 'sample.h'), ('a.out', 'a.out'), ('math.hpp', 'math.h'), ('hpp.out', 'hpp.out')]
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+newfilenames = [] 
+
+for orig in filenames:
+  tuple_file = ()
+  if '.hpp' in orig:
+    new = orig.replace('.hpp', '.h')
+    tuple_file = (orig, new)
+    newfilenames.append(tuple_file)
+  else:
+    tuple_file = (orig, orig)
+    newfilenames.append(tuple_file)
+    
+
+print (newfilenames) # Should be [('program.c', 'program.c'), ('stdio.hpp', 'stdio.h'), ('sample.hpp', 'sample.h'), ('a.out', 'a.out'), ('math.hpp', 'math.h'), ('hpp.out', 'hpp.out')]
+
+# ** go over again!
+# The permissions of a file in a Linux system are split into three sets of three permissions: read, write, and execute for the owner, group, and others.
+# Each of the three values can be expressed as an octal number summing each permission, with 4 corresponding to read, 2 to write, 
+# and 1 to execute. Or it can be written with a string using the letters r, w, and x or - when the permission is not granted. 
+# For example: 640 is read/write for the owner, read for the group, and no permissions for the others; 
+# converted to a string, it would be: "rw-r-----" 755 is read/write/execute for the owner, and read/execute for group and others;converted to a 
+# string, it would be: "rwxr-xr-x" Fill in the blanks to make the code convert a permission in octal format into a string format.
+
+def octal_to_string(octal):
+    result = ""
+    value_letters = [(4,"r"),(2,"w"),(1,"x")]
+    # Iterate over each of the digits in octal
+    for p in [int(n) for n in str(octal)]:
+        # Check for each of the permissions values
+        for value, letter in value_letters:
+            if p >= value:
+                result += letter
+                p -= value
+            else:
+                result += "-"
+    return result
+    
+print(octal_to_string(755)) # Should be rwxr-xr-x
+print(octal_to_string(644)) # Should be rw-r--r--
+print(octal_to_string(750)) # Should be rwxr-x---
+print(octal_to_string(600)) # Should be rw-------
+
+
+# Let's create a function that turns text into pig latin: a simple text transformation
+# that modifies each word moving the first character to the end and appending "ay" to the end. 
+# For example, python ends up as ythonpay.
+def pig_latin(text):
+  say = ""
+  # Separate the text into words
+  words = text.split(" ")
+  for word in words:
+    # print(word)
+    # Create the pig latin word and add it to the list
+    x = word[:1]
+    # Turn the list back into a phrase
+    y = word[1:] + x
+    y = y + 'ay'
+    y = y + " "
+    say += y
+  return (say)
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+
+# -- Dictionaries 
 # Now, it's your turn! Have a go at iterating over a dictionary!
 
 # Complete the code to iterate through the keys and values of the cool_beasts dictionary. 
