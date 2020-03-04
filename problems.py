040bc2cf14adf29a10cf0839c98f2af6f5960000
fergmack <33000542+fergmack@users.noreply.github.com>
2020-03-04 21:05:38 +0000

    Update problems.py

--- problems.py
+++ problems.py
@@ -485,3 +485,44 @@ print(groups_per_user({"local": ["admin", "userA"],
 		"public":  ["admin", "userB"],
 		"administrator": ["admin"] }))
 
+# -- Works but not the best solution probably -- 
+# The format_address function separates out parts of the address string into new strings: house_number and street_name, and returns:
+# "house number X on street named Y". The format of the input string is: numeric house number, followed by the street name which 
+# may contain numbers, but never by themselves, and could be several
+# words long. For example, "123 Main Street", "1001 1st Ave", or "55 North Center Drive". 
+# Fill in the gaps to complete this function.
+
+def format_address(address_string):
+  # Declare variables
+  num = 0
+  street = ''
+
+  # Separate the address string into parts
+  address_parts = address_string.split(' ')
+
+  # Traverse through the address parts
+  for part in address_parts:
+    # Determine if the address part is the
+    # house number or part of the street name
+    if part == address_parts[0]:
+      num = str(part)
+    else:
+      street += part 
+      street += ' ' 
+    
+
+  # Does anything else need to be done 
+  # before returning the result?
+  
+  # Return the formatted string  
+  return "house number {} on street named {}".format(num, street)
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
+
