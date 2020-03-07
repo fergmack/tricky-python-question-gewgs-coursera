6bda20330057473ad9a1024966adfcd2b4bedfa1
fergmack <33000542+fergmack@users.noreply.github.com>
2020-03-07 11:18:09 +0000

    Update problems.py

--- problems.py
+++ problems.py
@@ -555,3 +555,25 @@ Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
 Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
 
 print(combine_lists(Jamies_list, Drews_list))
+
+# Use a list comprehension to create a list of squared numbers (n*n). The function receives the variables start and end, 
+# and returns a list of squares of consecutive numbers between start and end inclusively. For example, squares(2, 3) should return [4, 9].
+def squares(start, end):
+  squares = []
+  for i in range(start, end+1):
+    sq = i * i
+    squares.append(sq)
+  return squares
+
+print(squares(2, 3)) # Should be [4, 9]
+print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Complete the code to iterate through the keys and values of the car_prices dictionary, printing out some information about each one.
+def car_listing(car_prices):
+  result = ""
+  for key, value in car_prices.items():
+    result += "{} costs {} dollars".format(key, value) + "\n"
+  return result
+
+print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
