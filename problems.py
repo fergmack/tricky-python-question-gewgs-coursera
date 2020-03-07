a187e563d738ab0c975520d5abb4d8cbc720e03b
fergmack <33000542+fergmack@users.noreply.github.com>
2020-03-07 11:02:43 +0000

    Update problems.py

--- problems.py
+++ problems.py
@@ -537,3 +537,21 @@ def highlight_word(sentence, word):
 print(highlight_word("Have a nice day", "nice"))
 print(highlight_word("Shhh, don't be so loud!", "loud"))
 print(highlight_word("Automating with Python is fun", "fun"))
+
+# A professor with two assistants, Jamie and Drew, wants an attendance list of the students, in the order that they arrived in the classroom.
+# Drew was the first one to note which students arrived, and then Jamie took over. After the class, they each entered their lists into the 
+# computer and emailed them to the professor, who needs to combine them into one, in the order of each student's arrival. Jamie emailed a
+# follow-up, saying that her list is in reverse order. Complete the steps to combine them into one list as 
+# follows: the contents of Drew's list, followed by Jamie's list in reverse order, to get an accurate list of the students as they arrived.
+def combine_lists(list1, list2):
+  # Generate a new list containing the elements of list2
+  # Followed by the elements of list1 in reverse order
+  # reverse the list using [::-1]
+  list1_reversed = list1[::-1]
+  full_list = list2 + list1_reversed
+  return full_list
+
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+
+print(combine_lists(Jamies_list, Drews_list))
