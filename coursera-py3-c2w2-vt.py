02623952f6fb22cbcfc0992e59f3cd5cd12e4084
fergmack <33000542+fergmack@users.noreply.github.com>
2020-03-25 19:08:13 +0000

    Update coursera-py3-c2w2-vt.py

--- coursera-py3-c2w2-vt.py
+++ coursera-py3-c2w2-vt.py
@@ -1,8 +1,6 @@
 # very tricky 
 # Write a program that finds the key in a dictionary that has the maximum value. If two keys have the same maximum value, it’s OK to print out either one. Fill in the skeleton code
 
-
-
 d = {'a': 1, 'b': 54, 'c':34, 'd': 44, 'e': 312, 'full':31}
 
 ks = d.keys()
@@ -52,3 +50,10 @@ for k in d:
     if lett_d[k] > lett_d[max_value]:
         max_value = k
 
+# Now that you have experience using lists and dictionaries, you will have to decide which one is best to use in each situation. The following guidelines will help you recognize when a dictionary will be beneficial:
+
+# When a piece of data consists of a set of properties of a single item, a dictionary is often better. You could try to keep track mentally that the zip code property is at index 2 in a list, but your code will be easier to read and you will make fewer mistakes if you can look up mydiction[‘zipcode’] than if you look up mylst[2].
+
+# When you have a collection of data pairs, and you will often have to look up one of the pairs based on its first value, it is better to use a dictionary than a list of (key, value) tuples. With a dictionary, you can find the value for any (key, value) tuple by looking up the key. With a list of tuples you would need to iterate through the list, examining each pair to see if it had the key that you want.
+
+# On the other hand, if you will have a collection of data pairs where multiple pairs share the same first data element, then you can’t use a dictionary, because a dictionary requires all the keys to be distinct from each other.
