4cef1f35ce3330cc2ea5fdf876236530edb8cbe3
fergmack <33000542+fergmack@users.noreply.github.com>
2020-03-25 19:05:47 +0000

    Update coursera-py3-c2w2-vt.py

--- coursera-py3-c2w2-vt.py
+++ coursera-py3-c2w2-vt.py
@@ -33,3 +33,22 @@ for k in keys:
         min_value = k 
         
 print(min_value)
+
+# Create a dictionary called lett_d that keeps track of all of the characters in the string product and notes how many times each character was seen. Then, find the key with the highest value in this dictionary and assign that key to max_value.
+
+product = "iphone and android phones"
+
+lett_d = {}
+for c in product:
+    if c not in lett_d:
+        lett_d[c] = 1
+    else:
+        lett_d[c] += 1
+
+d = lett_d.keys()
+max_value = list(d)[0]
+
+for k in d:
+    if lett_d[k] > lett_d[max_value]:
+        max_value = k
+
