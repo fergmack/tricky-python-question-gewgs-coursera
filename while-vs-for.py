fd8396f8d8c992c503d01399bc8e3f38bfc15e99
fergmack <33000542+fergmack@users.noreply.github.com>
2020-05-17 20:30:06 +0100

    Create while-vs-for.py

---
+++ while-vs-for.py
@@ -0,0 +1,14 @@
+Below, we’ve provided a for loop that sums all the elements of list1. Write code that accomplishes the same task, but instead uses a while loop. Assign the accumulator variable to the name accum.
+
+list1 = [8, 3, 4, 5, 6, 7, 9]
+
+tot = 0
+for elem in list1:
+    tot = tot + elem
+
+
+accum = 0 
+n = 0
+while n < len(list1):
+    accum += list1[n]
+    n += 1
