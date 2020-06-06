b47c30d274900494940419594b0bd00c50ad8da0
fergmack <33000542+fergmack@users.noreply.github.com>
2020-06-06 14:36:04 +0100

    Delete remove-punctuation.py

--- remove-punctuation.py
+++
@@ -1,5 +0,0 @@
-import string
-
-s = "string. With. Punctuation?"
-table = str.maketrans(dict.fromkeys(string.punctuation))  # OR {key: None for key in string.punctuation}
-new_s = s.translate(table) 
