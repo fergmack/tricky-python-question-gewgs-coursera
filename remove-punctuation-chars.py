3bb327fb3d3f361965128ca05421ce1f8546b3ff
fergmack <33000542+fergmack@users.noreply.github.com>
2020-06-06 14:35:45 +0100

    Create remove-punctuation-chars.py

---
+++ remove-punctuation-chars.py
@@ -0,0 +1,16 @@
+Write a function named string_processing that takes a list of strings as input and returns an all-lowercase string with no punctuation.
+
+There should be a space between each word. You do not have to check for edge cases.
+
+
+
+import string
+def string_processing(string_list):
+  str1 = ""
+  for ele in string_list:
+    str1 += " " 
+    str1 += ele
+  str2 = str1.lower()
+  table = str.maketrans(dict.fromkeys(string.punctuation))
+  return str2.translate(table)
+  
