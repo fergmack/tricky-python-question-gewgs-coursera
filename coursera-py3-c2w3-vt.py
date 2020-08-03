274495c0a48176ee891d31efc2f3bfb966e237f0
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-03 20:26:10 +0100

    Create coursera-py3-c2w3-vt.py

---
+++ coursera-py3-c2w3-vt.py
@@ -0,0 +1,2 @@
+//** Really Pointless Qusetion Below - Just use a for loop **
+Write a function, sublist, that takes in a list of numbers as the parameter. In the function, use a while loop to return a sublist of the input list. The sublist should contain the same values of the original list up until it reaches the number 5 (it should not contain the number 5).
