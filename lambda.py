5e614a778fa84718f10c36c857ea0488082a08f7
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-05 18:54:55 +0100

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,3 @@
+A function, whether named or anonymous, can be called by placing parentheses () after it.
+The lambda expression had to go in parentheses just for the purposes of grouping all its contents together. 
+Without the extra parentheses around it on line 10, the interpreter would group things differently and make a function of x that returns x - 2(6).
