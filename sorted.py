c4b230bcdbe6748bc89dab7890953ed5b5ce8ac8
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-08 00:35:03 +0100

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -1,4 +1,4 @@
-we will generally use an alternative way of sorting, the function sorted rather than the method sort. Because it is a function rather than a method, it is invoked on a list by passing the list as a parameter inside the parentheses, rather than putting the list before the period. More importantly, sorted does not change the original list. Instead, it returns a new list.
+# We will generally use an alternative way of sorting, the function sorted rather than the method sort. Because it is a function rather than a method, it is invoked on a list by passing the list as a parameter inside the parentheses, rather than putting the list before the period. More importantly, sorted does not change the original list. Instead, it returns a new list.
 
 L2 = ["Cherry", "Apple", "Blueberry"]
 
