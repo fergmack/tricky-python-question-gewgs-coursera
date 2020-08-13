7a606ab473438dd602a1056016d29ebefff952c0
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-14 00:08:15 +0100

    Update breaking-ties-second-sorting.py

--- breaking-ties-second-sorting.py
+++ breaking-ties-second-sorting.py
@@ -1,6 +1,44 @@
 # In the code below, we are going to sort a list of fruit words first by their length, smallest to largest, and then alphabetically to break ties among words of the same length. To do that, we have the key function return a tuple whose first element is the length of the fruit’s name, and second element is the fruit name itself.
+#  Here, each word is evaluated first on it’s length, then by its alphabetical order. Note that we could continue to specify other conditions by including more elements in the tuple.
 
 fruits = ['peach', 'kiwi', 'apple', 'blueberry', 'papaya', 'mango', 'pear']
 new_order = sorted(fruits, key=lambda fruit_name: (len(fruit_name), fruit_name))
 for fruit in new_order:
     print(fruit)
+    
+# What would happen though if we wanted to sort it by largest to smallest, and then by alphabetical order?
+
+fruits = ['peach', 'kiwi', 'apple', 'blueberry', 'papaya', 'mango', 'pear']
+new_order = sorted(fruits, key=lambda fruit_name: (len(fruit_name), fruit_name), reverse=True)
+for fruit in new_order:
+    print(fruit)
+
+# Do you see a problem here? Not only does it sort the words from largest to smallest, but also in reverse alphabetical order! Can you think of any ways you can solve this issue?
+
+# One solution is to add a negative sign in front of len(fruit_name), which will convert all positive numbers to negative, and all negative numbers to positive. As a result, the longest elements would be first and the shortest elements would be last.
+# Note no reverse=True is used
+fruits = ['peach', 'kiwi', 'apple', 'blueberry', 'papaya', 'mango', 'pear']
+new_order = sorted(fruits, key=lambda fruit_name: (-len(fruit_name), fruit_name))
+for fruit in new_order:
+    print(fruit)
+
+#     What will the sorted function sort by?
+# Ans =  first city name (alphabetically), then temperature (lowest to highest)
+weather = {'Reykjavik': {'temp':60, 'condition': 'rainy'},
+           'Buenos Aires': {'temp': 55, 'condition': 'cloudy'},
+           'Cairo': {'temp': 96, 'condition': 'sunny'},
+           'Berlin': {'temp': 89, 'condition': 'sunny'},
+           'Caloocan': {'temp': 78, 'condition': 'sunny'}}
+
+sorted_weather = sorted(weather, key=lambda w: (w, weather[w]['temp']))
+
+# What how will the following data be sorted?
+# first city name (reverse alphabetically) Z-A, then temperature (lowest to highest) as the high temps are now negatives 
+weather = {'Reykjavik': {'temp':60, 'condition': 'rainy'},
+           'Buenos Aires': {'temp': 55, 'condition': 'cloudy'},
+           'Cairo': {'temp': 96, 'condition': 'sunny'},
+           'Berlin': {'temp': 89, 'condition': 'sunny'},
+           'Caloocan': {'temp': 78, 'condition': 'sunny'}}
+
+sorted_weather = sorted(weather, key=lambda w: (w, -weather[w]['temp']), reverse=True)
+
