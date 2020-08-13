0c2600d6b7335176196ae8237000b235e8d3ccb0
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-13 23:47:21 +0100

    Create breaking-ties-second-sorting.py

---
+++ breaking-ties-second-sorting.py
@@ -0,0 +1,2 @@
+In the code below, we are going to sort a list of fruit words first by their length, smallest to largest, and then alphabetically to break ties among words of the same length. To do that, we have the key function return a tuple whose first element is the length of the fruit’s name, and second element is the fruit name itself.
+
