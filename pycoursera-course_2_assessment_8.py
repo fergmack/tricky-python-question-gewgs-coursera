241e3ef69b5ba9b6e2920eaa33f1614b4267ad2c
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-16 22:27:15 +0100

    Update pycoursera-course_2_assessment_8.py

--- pycoursera-course_2_assessment_8.py
+++ pycoursera-course_2_assessment_8.py
@@ -10,10 +10,27 @@ animals_sorted = sorted(animals)
 
 # The dictionary, medals, shows the medal count for six countries during the Rio Olympics. Sort the country names so they appear alphabetically. Save this list to the variable alphabetical.
 medals = {'Japan':41, 'Russia':56, 'South Korea':21, 'United States':121, 'Germany':42, 'China':70}
-alphabetical = sorted(medal
+alphabetical = sorted(medal)
 
 # Given the same dictionary, medals, now sort by the medal count. Save the three countries with the highest medal count to the list, top_three.
 medals = {'Japan':41, 'Russia':56, 'South Korea':21, 'United States':121, 'Germany':42, 'China':70}
 
 top_three = sorted(medals, key = lambda medal_count: medals[medal_count], 
                     reverse=True)[:3]
+
+# We have provided the dictionary groceries. You should return a list of its keys, but they should be sorted by their values, from highest to lowest. Save the new list as most_needed.
+
+groceries = {'apples': 5, 'pasta': 3, 'carrots': 12, 'orange juice': 2, 'bananas': 8, 'popcorn': 1, 'salsa': 3, 'cereal': 4, 'coffee': 5, 'granola bars': 15, 'onions': 7, 'rice': 1, 'peanut butter': 2, 'spinach': 9}
+most_needed = sorted(groceries, key=lambda item: groceries[item], reverse=True)
+
+# Create a function called last_four that takes in an ID number and returns the last four digits. For example, the number 17573005 should return 3005. Then, use this function to sort the list of ids stored in the variable, ids, from lowest to highest. Save this sorted list in the variable, sorted_ids. Hint: Remember that only strings can be indexed, so conversions may be needed.
+# Note: just need to pass the name of the function to key
+def last_four(x):
+    return str(x)[-4:]
+
+ids = [17573005, 17572342, 17579000, 17570002, 17572345, 17579329]
+
+sorted_ids = sorted(ids, key = last_four )
+
+
+
