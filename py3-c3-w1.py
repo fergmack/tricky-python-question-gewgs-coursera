242a304cf14e6395420fd9bb54f37ad55ed4a839
fergmack <33000542+fergmack@users.noreply.github.com>
2020-09-23 19:51:07 +0100

    Update py3-c3-w1.py

--- py3-c3-w1.py
+++ py3-c3-w1.py
@@ -1,3 +1,4 @@
+# -- 1 
 nested2 = [{'a': 1, 'b': 3}, {'a': 5, 'c': 90, 5: 50}, {'b': 3, 'c': "yes"}]
 
 #write code to print the value associated with key 'c' in the second dictionary (90)
@@ -10,3 +11,21 @@ print(nested2[3])
 #change the value associated with 'c' in the third dictionary from "yes" to "no"; print something to check your work
 nested2[2]['c'] = 'no'
 print(nested2)
+
+# -- 2 - you can have functions in a list and call them
+def square(x):
+    return x*x
+
+L = [square, abs, lambda x: x+1]
+
+print("****names****")
+for f in L:
+    print(f)
+
+print("****call each of them****")
+for f in L:
+    print(f(-2))
+
+print("****just the first one in the list****")
+print(L[0])
+print(L[0](3))
