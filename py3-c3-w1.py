b02ffe65f1dfc06050f7c5e818a2bebf8ab5dda9
fergmack <33000542+fergmack@users.noreply.github.com>
2020-09-23 19:57:48 +0100

    Update py3-c3-w1.py

--- py3-c3-w1.py
+++ py3-c3-w1.py
@@ -29,3 +29,18 @@ for f in L:
 print("****just the first one in the list****")
 print(L[0])
 print(L[0](3))
+
+# 1. Below, we have provided a list of lists. Use indexing to assign the element ‘horse’ to the variable name idx1.
+animals = [['cat', 'dog', 'mouse'], ['horse', 'cow', 'goat'], ['cheetah', 'giraffe', 'rhino']]
+idx1 = animals[1][0]
+
+# 2. Using indexing, retrieve the string ‘willow’ from the list and assign that to the variable plant.
+data = ['bagel', 'cream cheese', 'breakfast', 'grits', 'eggs', 'bacon',
+        [34, 9, 73, []],
+        [['willow', 'birch', 'elm'],
+         'apple', 'peach', 'cherry']]
+plant = data[7][0][0]
+
+
+
+
