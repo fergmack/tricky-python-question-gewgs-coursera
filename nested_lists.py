4b6e4e7f234602b53272bb8a4aca29dcb1b9bed6
fergmack <33000542+fergmack@users.noreply.github.com>
2020-10-27 00:44:52 +0000

    Create nested_lists.py

---
+++ nested_lists.py
@@ -0,0 +1,10 @@
+# dealing with diffrent types - i.e. a mixture of ints and lists in a list
+
+nested1 = [1, 2, ['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+for x in nested1:
+    print("level1: ")
+    if type(x) is list:
+        for y in x:
+            print("     level2: {}".format(y))
+    else:
+        print(x)
