133432744278bfcfbb5ca128f8fc4c7c506976d6
fergmack <33000542+fergmack@users.noreply.github.com>
2020-10-30 11:41:36 +0000

    Update course3_w1_finalAssesment.py

--- course3_w1_finalAssesment.py
+++ course3_w1_finalAssesment.py
@@ -34,7 +34,7 @@ else:
 
  
 # NOTE USING A SUPER SHORT WAY
-# Use in statements to create variables with Boolean values - see the ActiveCode window for further directions.
+# 3. Use in statements to create variables with Boolean values - see the ActiveCode window for further directions.
 
 L = [[5, 8, 7], ['hello', 'hi', 'hola'], [6.6, 1.54, 3.99], ['small', 'large']]
 
@@ -45,6 +45,17 @@ test1 = 'hola' in L
 test2 = [5, 8, 7] in L
 # Test if 6.6 is in the third element of list L. Save to variable name test3
 test3 = 6.6 in L[2]
+
+# 4. Provided is a nested data structure. Follow the instructions in the comments below. Do not hard code.
+# Check to see if the string data is a key in nested, if it is, assign True to the variable data, otherwise assign False.
+data = 'data' in nested.keys()
+# Check to see if the integer 24 is in the value of the key data, if it is then assign to the variable twentyfour the value of True, otherwise False.
+twentyfour = 24 in nested.values()
+# Check to see that the string 'whole' is not in the value of the key window. If it's not, then assign to the variable whole the value of True, otherwise False.
+whole = 'whole' in nested.values()
+# Check to see if the string 'physics' is a key in the dictionary nested. If it is, assign to the variable physics, the value of True, otherwise False.
+physics = 'pyhsics' in nested.values()
+
 
 
 
