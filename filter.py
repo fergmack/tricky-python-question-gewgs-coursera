e2433641ae6560871915a09e7a7bb4fa1cc6d6c7
fergmack <33000542+fergmack@users.noreply.github.com>
2020-11-11 20:47:02 +0000

    Update filter.py

--- filter.py
+++ filter.py
@@ -16,3 +16,16 @@ def keep_evens(nums):
     return list(new_seq)
 
 print(keep_evens([3, 4, 6, 7, 0, 1]))
+
+# 1. Write code to assign to the variable filter_testing all the elements in lst_check that have a w in them using filter.
+
+lst_check = ['plums', 'watermelon', 'kiwi', 'strawberries', 'blueberries', 'peaches', 'apples', 'mangos', 'papaya']
+
+filter_testing  = filter( lambda x: 'w' in x, lst_check) 
+
+# 2. Using filter, filter lst so that it only contains words containing the letter “o”. Assign to variable lst2. Do not hardcode this.
+
+
+lst = ["witch", "halloween", "pumpkin", "cat", "candy", "wagon", "moon"]
+
+lst2 = filter( lambda x:  'o' in x, lst) 
