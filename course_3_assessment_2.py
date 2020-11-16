ce323aa7a288d3b413d172761e9a388e1a7a7a15
fergmack <33000542+fergmack@users.noreply.github.com>
2020-11-16 21:23:32 +0000

    Update course_3_assessment_2.py

--- course_3_assessment_2.py
+++ course_3_assessment_2.py
@@ -1,1 +1,54 @@
+# List comprehension: [<expression> <input sequence> <conditional>]
 
+# 1. Write code to assign to the variable map_testing all the elements in lst_check while adding the string “Fruit: ” to the beginning of each element using mapping.
+
+lst_check = ['plums', 'watermelon', 'kiwi', 'strawberries', 'blueberries', 'peaches', 'apples', 'mangos', 'papaya']
+
+map_testing = map(lambda x: 'Fruit: ' + x, lst_check)
+
+# 2. Below, we have provided a list of strings called countries. Use filter to produce a list called b_countries that only contains the strings from countries that begin with B.
+
+countries = ['Canada', 'Mexico', 'Brazil', 'Chile', 'Denmark', 'Botswana', 'Spain', 'Britain', 'Portugal', 'Russia', 'Thailand', 'Bangladesh', 'Nigeria', 'Argentina', 'Belarus', 'Laos', 'Australia', 'Panama', 'Egypt', 'Morocco', 'Switzerland', 'Belgium']
+
+b_countries = filter(lambda x: x[0] == 'B', countries)
+
+# 3. Below, we have provided a list of tuples that contain the names of Game of Thrones characters. Using list comprehension, create a list of strings called first_names that contains only the first names of everyone in the original list.
+people = [('Snow', 'Jon'), ('Lannister', 'Cersei'), ('Stark', 'Arya'), ('Stark', 'Robb'), ('Lannister', 'Jamie'), ('Targaryen', 'Daenerys'), ('Stark', 'Sansa'), ('Tyrell', 'Margaery'), ('Stark', 'Eddard'), ('Lannister', 'Tyrion'), ('Baratheon', 'Joffrey'), ('Bolton', 'Ramsey'), ('Baelish', 'Peter')]
+
+first_names = [x[1] for x in people]
+
+# 4. Use list comprehension to create a list called lst2 that doubles each element in the list, lst.
+lst = [["hi", "bye"], "hello", "goodbye", [9, 2], 4]
+lst2 = [x*2 for x in lst]
+
+# 5. Below, we have provided a list of tuples that contain students’ names and their final grades in PYTHON 101. Using list comprehension, create a new list passed that contains the names of students who passed the class (had a final grade of 70 or greater).
+students = [('Tommy', 95), ('Linda', 63), ('Carl', 70), ('Bob', 100), ('Raymond', 50), ('Sue', 75)]
+
+passed = [x[0] for x in students if x[1] >= 70]
+
+# 6. Below, we have provided a list of tuples that contain students’ names and their final grades in PYTHON 101. Using list comprehension, create a new list passed that contains the names of students who passed the class (had a final grade of 70 or greater).
+
+students = [('Tommy', 95), ('Linda', 63), ('Carl', 70), ('Bob', 100), ('Raymond', 50), ('Sue', 75)]
+
+passed = [x[0] for x in students if x[1] >= 70]
+
+
+#  7. Write code using zip and filter so that these lists (l1 and l2) are combined into one big list and assigned to the variable opposites if they are both longer than 3 characters each.
+
+
+l1 = ['left', 'up', 'front']
+l2 = ['right', 'down', 'back']
+
+opposites = filter(lambda x: len(x[0]) > 3 and len(x[1]), zip(l1, l2) )
+
+# 8. Below, we have provided a species list and a population list. Use zip to combine these lists into one list of tuples called pop_info. From this list, create a new list called endangered that contains the names of species whose populations are below 2500.
+species = ['golden retriever', 'white tailed deer', 'black rhino', 'brown squirrel', 'field mouse', 'orangutan', 'sumatran elephant', 'rainbow trout', 'black bear', 'blue whale', 'water moccasin', 'giant panda', 'green turtle', 'blue jay', 'japanese beetle']
+
+population = [10000, 90000, 1000, 2000000, 500000, 500, 1200, 8000, 12000, 2300, 7500, 100, 1800, 9500, 125000]
+
+pop_info = zip(species, population)
+
+endangered = [x1 for (x1, x2) in pop_info if x2 < 2500]
+
+# BONUS Remember below for dictionaries - search through all the values in the dict, then for item['name'] key for each item in x
+[ [item['name'] for item in x ] for x in dict.values()[0]
